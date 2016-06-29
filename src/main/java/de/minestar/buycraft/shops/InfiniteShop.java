942c06d700b31206b084e749f65aefeb64ebee41
Stephan Ihling <me@outi-networks.de>
2016-06-29 22:22:36 +0200

    update to 1.10.0

--- src/main/java/de/minestar/buycraft/shops/InfiniteShop.java
+++ src/main/java/de/minestar/buycraft/shops/InfiniteShop.java
@@ -2,7 +2,7 @@ package de.minestar.buycraft.shops;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
