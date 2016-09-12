bccaf96a81f917d87c4ef78d6a66497e68ffce1f
Stephan Ihling <me@outi-networks.de>
2016-09-12 09:30:16 +0200

    fixed utf8

--- src/main/java/de/minestar/buycraft/core/Messages.java
+++ src/main/java/de/minestar/buycraft/core/Messages.java
@@ -7,10 +7,10 @@ public class Messages {
 
     public static String INFINITE_SHOP_CREATE_ERROR = "Du kannst keine Servershops erstellen.";
     public static String INFINITE_SHOP_CREATE_SUCCESS = "Servershop erstellt.";
-    public static String INFINITE_SHOP_CREATE_SUCCESS_COMPLETE = "Servershop vollständig erstellt.";
+    public static String INFINITE_SHOP_CREATE_SUCCESS_COMPLETE = "Servershop vollstÃ¤ndig erstellt.";
 
     // USERSHOPS
-    public static String NAME_IS_TOO_LONG = "Dein Name ist zu lang für Usershops. Bitte wende dich an einen Admin!";
+    public static String NAME_IS_TOO_LONG = "Dein Name ist zu lang fÃ¼r Usershops. Bitte wende dich an einen Admin!";
 
     public static String USER_SHOP_DESTROY_ERROR_THIS = "Du kannst diesen Usershops nicht entfernen.";
     public static String USER_SHOP_DESTROY_ERROR = "Du kannst keine Usershops entfernen.";
@@ -22,9 +22,9 @@ public class Messages {
     public static String USER_SHOP_DEACTIVATED = "Usershop deaktiviert.";
 
     public static String USER_SHOP_CREATE_ERROR = "Du kannst keine Usershops erstellen.";
-    public static String USER_SHOP_CREATE_FOR_OTHERS_ERROR = "Du kannst keine Usershops für andere Spieler erstellen.";
+    public static String USER_SHOP_CREATE_FOR_OTHERS_ERROR = "Du kannst keine Usershops fÃ¼r andere Spieler erstellen.";
     public static String USER_SHOP_CREATE_SUCCESS = "Usershop erstellt.";
-    public static String USER_SHOP_CREATE_SUCCESS_COMPLETE = "Usershop vollständig erstellt.";
+    public static String USER_SHOP_CREATE_SUCCESS_COMPLETE = "Usershop vollstÃ¤ndig erstellt.";
 
     public static String USER_SHOP_INTERNAL_ERROR_0X00 = "Ein interner Fehler ist aufgetreten [0x00]. [Usershop expected, but not found]";
     public static String USER_SHOP_INTERNAL_ERROR_0X01 = "Ein interner Fehler ist aufgetreten [0x01]. [Could not create Usershop in DB]";
@@ -32,7 +32,7 @@ public class Messages {
     public static String USER_SHOP_INTERNAL_ERROR_0X03 = "Ein interner Fehler ist aufgetreten [0x03]. [Could not finish Usershop in DB]";
     public static String USER_SHOP_INTERNAL_ERROR_0X04 = "Ein interner Fehler ist aufgetreten [0x04]. [Could not remove Usershop in DB]";
 
-    public static String ADMINS_MUST_SNEAK = "Admins müssen schleichen um einen fremden Usershop zu deaktivieren.";
+    public static String ADMINS_MUST_SNEAK = "Admins mÃ¼ssen schleichen um einen fremden Usershop zu deaktivieren.";
 
     // ALIASES
     public static String NO_ALIASES_SET = "Bisher wurden keine Aliasnamen gesetzt.";
@@ -41,8 +41,8 @@ public class Messages {
     public static String ALIAS_CREATED = "Der Aliasname wurde erfolgreich angelegt.";
     public static String ALIAS_CREATED_ERROR = "Der Aliasname konnte nicht angelegt werden!";
 
-    public static String ALIAS_DELETED = "Der Aliasname wurde erfolgreich gelöscht.";
-    public static String ALIAS_DELETED_ERROR = "Der Aliasname konnte nicht gelöscht werden!";
+    public static String ALIAS_DELETED = "Der Aliasname wurde erfolgreich gelÃ¶scht.";
+    public static String ALIAS_DELETED_ERROR = "Der Aliasname konnte nicht gelÃ¶scht werden!";
 
     public static String ALIAS_EXISTS_PLAYER = "Der Spieler hat schon einen Aliasnamen!";
     public static String ALIAS_EXISTS_ALIAS = "Der Aliasname ist schon vergeben!";
@@ -58,9 +58,9 @@ public class Messages {
     public static String TRY_AGAIN_OR_CONTACT_ADMIN = "Versuche es erneut oder kontaktiere einen Admin.";
 
     public static String ITEM_NOT_ALLOWED = "Dieses Item ist nicht zugelassen.";
-    public static String WRONG_SYNTAX = "Unzulässige Syntax.";
+    public static String WRONG_SYNTAX = "UnzulÃ¤ssige Syntax.";
 
-    public static String NO_ITEM_IN_HAND = "Du musst das gewünschte Item in die Hand nehmen.";
+    public static String NO_ITEM_IN_HAND = "Du musst das gewÃ¼nschte Item in die Hand nehmen.";
     public static String DESTROY_SIGN_FIRST = "Das Schild muss zuerst entfernt werden!";
     public static String CREATE_CHEST_FIRST = "Du musst zuerst eine Kiste erstellen!";
     public static String DOUBLE_CHESTS_NOT_ALLOWED = "Doppelkisten sind nicht erlaubt!";
